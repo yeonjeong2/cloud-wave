0c937ff01172f3c87c12fff3310af8b9a206d24e
Workshop Public Cloud <cj-cloud-wave@workshops.aws>
2025-02-04 05:26:45 +0000

    update main page

--- main.py
+++ main.py
@@ -20,22 +20,22 @@ if 'logged_in' not in st.session_state:
     # st.session_state['logged_in'] = False
 
 def side_bar():
-    with st.sidebar.container():
-        selected_page = option_menu(
-            menu_title="Main Menu", 
-            options=["Main_Home"],
+#    with st.sidebar.container():
+#        selected_page = option_menu(
+#            menu_title="Main Menu", 
+#            options=["Main_Home"],
             # options=["Main_Home", "Photo_Gallery"],
             # options=["Main_Home", "Photo_Gallery", "Travel_Attractions"],
-            icons=['bi bi-house', 'bi bi-images', 'bi bi-airplane-engines'],
-            menu_icon="cast", 
-            default_index=0,
-            styles={
-                "container": {"padding": "4!important", "background-color": "#fafafa"},
-                "icon": {"color": "black", "font-size": "20px"},
-                "nav-link": {"font-size": "14px", "text-align": "left", "margin":"0px", "--hover-color": "#fafafa"},
-                "nav-link-selected": {"background-color": "#08c7b4"}
-            }
-        )
+#            icons=['bi bi-house', 'bi bi-images', 'bi bi-airplane-engines'],
+#            menu_icon="cast", 
+#            default_index=0,
+#            styles={
+#                "container": {"padding": "4!important", "background-color": "#fafafa"},
+#                "icon": {"color": "black", "font-size": "20px"},
+#                "nav-link": {"font-size": "14px", "text-align": "left", "margin":"0px", "--hover-color": "#fafafa"},
+#                "nav-link-selected": {"background-color": "#08c7b4"}
+#            }
+#        )
         # col1, col2, col3 = st.columns(3)
         # with col1:
         #     # RDS Database Login Session / Logout Function      
@@ -45,7 +45,7 @@ def side_bar():
         # with col2:
         #     if st.button('ReRun', use_container_width=True):
         #         st.rerun()
-    return selected_page
+#    return selected_page
 
 def main():
     if st.session_state['logged_in'] == True:
